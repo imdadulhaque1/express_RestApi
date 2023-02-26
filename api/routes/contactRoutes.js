7453b7e075ebe4b296250e699d3e7145ec3b557d
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-02-26 11:54:27 +0600

    Stored data showed by get method

--- api/routes/contactRoutes.js
+++ api/routes/contactRoutes.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 const ContactModel = require("../models/ContactModel");
 
-router.get("/", (req, res, next) => {});
+router.get("/", (req, res, next) => {
+  ContactModel.find()
+    .then((data) => {
+      res.status(200).json({
+        message: "All Contacts Showed!!!",
+        contacts: data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error Occured!!!",
+        error: err,
+      });
+    });
+});
 
 router.post("/", (req, res, next) => {
   const contact = new ContactModel({
@@ -19,7 +34,13 @@ router.post("/", (req, res, next) => {
         contact: data,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error Occured!!!",
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
